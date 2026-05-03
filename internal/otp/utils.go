c9b41e6af4a5388819f6e1008b3874112e750b5b
Nidal Bakir <nidal.bakir.edu@gmail.com>
2026-05-03 12:39:23 +0300

    feat: user sha3 to hash the otp

--- internal/otp/utils.go
+++ internal/otp/utils.go
@@ -1,13 +1,13 @@
 package otp
 
 import (
-	"crypto/sha256"
+	"crypto/sha3"
 	"fmt"
 )
 
-// Hashes the OTP using SHA-256 and encodes the result as a hexadecimal string.
+// Hashes the OTP using SHA3-256 and encodes the result as a hexadecimal string.
 // Do not modify this function without proper preparation, as it will break
 // OTP compatibility with currently deployed systems.
 func HashOtp(otp string) string {
-	return fmt.Sprintf("%x", sha256.Sum256([]byte(otp)))
+	return fmt.Sprintf("%x", sha3.Sum256([]byte(otp)))
 }
